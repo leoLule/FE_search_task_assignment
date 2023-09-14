86a170931e5bb470ce77d0aef10bad20aa52fba0
Lukas Lehrmann <121244965+leoLule@users.noreply.github.com>
2023-09-14 12:03:34 +0300

    added context and fetch, have the productlist in teh console

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react';
-
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import ProductContextProvider from './context/ProductContext';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <>
-      <h1>h1</h1>
+      <ProductContextProvider>
+        {' '}
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+        </Routes>
+      </ProductContextProvider>
     </>
   );
 }
---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
+
+const ProductContext = createContext();
+const ProductContextProvider = ({ children }) => {
+  const [productList, setProductList] = useState([]);
+
+  const fetchAllProducts = async () => {
+    try {
+      const res = await axios.get('http://localhost:8080/products', {
+        // headers: { Authorization: `Bearer ${token}` },
+      });
+      setProductList(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider
+      value={{
+        setProductList,
+        productList,
+        fetchAllProducts,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export { ProductContext };
+export default ProductContextProvider;
