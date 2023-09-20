238bbe06f8d2e3ac947d20df8cb1301f7424d3aa
Lukas Lehrmann <121244965+leoLule@users.noreply.github.com>
2023-09-20 14:36:26 +0300

    added readme and working on styling, before implenting grid

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Used libraries and dependencies
+
+I used the following libraries and dependencies in my project:
+
+"MUI" - react helps with styling and positioning , a component library
+
+"axios": for making HTTP requests , allowing me to fetch data from APIs and interact with external resources.
+
+"react" and "react-dom": These are fundamental React libraries - used for building the user interface and managing the application's component tree.
+
+"react-router-dom": crucial for implementing client-side routing, ensuring that different views and pages can be navigated and rendered without a full page reload
+
+The combination of these libraries and dependencies helped me efficiently create a well-styled, responsive, and interactive web application with seamless navigation and data fetching capabilities.
--- src/components/ChangeScore.jsx
+++ src/components/ChangeScore.jsx
@@ -3,17 +3,25 @@ import { ProductContext } from '../context/ProductContext';
 import Product from './Product';
 import axios from 'axios';
 
-//logic for button increment , need the id of product , try to do it like toggleAdmin
 const ChangeScore = () => {
-  const { productList } = useContext(ProductContext);
-  const [incrementScore, setIncrementScore] = useState(''); // not sure if [] or " " or {}
+  const { productList, setProductList } = useContext(ProductContext);
 
   const incrementScoreFetch = async (id) => {
     try {
       const res = await axios.put('http://localhost:8080/products/increment', {
         id,
       });
-      setIncrementScore(res.data);
+
+      // Update the productList with the updated score
+      const updatedProductList = productList.map((product) => {
+        if (product.id === id) {
+          return { ...product, score: res.data.score };
+        }
+        return product;
+      });
+
+      // Update the state to re-render the component with the updated score
+      setProductList(updatedProductList);
     } catch (err) {
       console.log(err);
     }
@@ -21,14 +29,15 @@ const ChangeScore = () => {
 
   return (
     <>
-      <p>ChangeScore</p>
+      <h2>Increment Score</h2>
       <div className="change-score-images">
         {productList.map((product) => (
           <div key={product.id}>
             <Product product={product} />
             <button onClick={() => incrementScoreFetch(product.id)}>
-              Change Score +
+              Increment Score +
             </button>
+            <p>{product.score}</p>
           </div>
         ))}
       </div>
