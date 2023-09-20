2f680e58e05586d3c05dd3d4f8ad161b562da6b8
Lukas Lehrmann <121244965+leoLule@users.noreply.github.com>
2023-09-20 15:54:39 +0300

    added fallback

--- src/components/ChangeScore.jsx
+++ src/components/ChangeScore.jsx
@@ -1,7 +1,57 @@
+// import React, { useContext, useEffect, useState } from 'react';
+// import { ProductContext } from '../context/ProductContext';
+// import Product from './Product';
+// import axios from 'axios';
+// import Grid from '@mui/material/Grid';
+
+// const ChangeScore = () => {
+//   const { productList, setProductList } = useContext(ProductContext);
+
+//   const incrementScoreFetch = async (id) => {
+//     try {
+//       const res = await axios.put('http://localhost:8080/products/increment', {
+//         id,
+//       });
+
+//       // Update the productList with the updated score
+//       const updatedProductList = productList.map((product) => {
+//         if (product.id === id) {
+//           return { ...product, score: res.data.score };
+//         }
+//         return product;
+//       });
+
+//       // Update the state to re-render the component with the updated score
+//       setProductList(updatedProductList);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <h2>Increment Score</h2>
+//       <Grid container spacing={2} className="change-score-images">
+//         {productList.map((product) => (
+//           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+//             <Product product={product} />
+//             <button onClick={() => incrementScoreFetch(product.id)}>
+//               Increment Score +
+//             </button>
+//             <p>{product.score}</p>
+//           </Grid>
+//         ))}
+//       </Grid>
+//     </>
+//   );
+// };
+
+// export default ChangeScore;
 import React, { useContext, useEffect, useState } from 'react';
 import { ProductContext } from '../context/ProductContext';
 import Product from './Product';
 import axios from 'axios';
+import Grid from '@mui/material/Grid';
 
 const ChangeScore = () => {
   const { productList, setProductList } = useContext(ProductContext);
@@ -30,17 +80,18 @@ const ChangeScore = () => {
   return (
     <>
       <h2>Increment Score</h2>
-      <div className="change-score-images">
+      <Grid container spacing={2} className="change-score-images">
         {productList.map((product) => (
-          <div key={product.id}>
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={product} />
+
             <button onClick={() => incrementScoreFetch(product.id)}>
               Increment Score +
             </button>
             <p>{product.score}</p>
-          </div>
+          </Grid>
         ))}
-      </div>
+      </Grid>
     </>
   );
 };
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ProductContext } from '../context/ProductContext';
+import fallback from '../assets/fallback.jpeg';
 
 const Product = ({ product }) => {
   const { getDirectImageUrl } = useContext(ProductContext);
+  const [src, setSrc] = useState(getDirectImageUrl(product.path));
+
+  const handleImageError = (event) => {
+    setSrc(fallback);
+  };
+
   return (
     <div className="random-image" key={product.id}>
       <img
-        src={getDirectImageUrl(product.path)}
+        src={src}
         alt={`Image for ${product.id}`}
+        onError={handleImageError}
         width="200px"
         height="150px"
       />
